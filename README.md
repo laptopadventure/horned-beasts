843656da514dff2e2c58c3f08e67b558045820f2
laptopadventure <bazelart13@gmail.com>
2022-03-01 21:17:09 -0800

    feature requests complete

---
+++ README.md
@@ -0,0 +1,35 @@
+
+# Horned Beasts
+
+First react app.
+
+## Attributions
+
+All images are sourced from pexels, which offers CC0-licensed images.
+
+## Feature Reports
+
+
+Number and name of feature: #1 Display images
+Estimate of time needed to complete: This is being added posthumously, so I can't estimate.
+Start time: 3:00pm
+Finish time: 3:25pm
+Actual time needed to complete: ~25 minutes? I may be getting the start time wrong.
+
+Number and name of feature: #2 Allow users to vote for their favorite beast
+Estimate of time needed to complete: 1 hour
+Start time: 8:35pm
+Finish time: 8:40pm
+Actual time needed to complete: 5 minutes. Okay, I should have seen that coming.
+
+Number and name of feature: #3 Bootstrap
+Estimate of time needed to complete: 30 minutes
+Start time: 8:42pm
+Finish time: 9:03pm
+Actual time needed to complete: 21 minutes. A far better prediction this time around.
+
+Number and name of feature: #4 Json data mapped into Horned beasts
+Estimate of time needed to complete: 5 minutes
+Start time: 9:06pm
+Finish time: 9:12pm
+Actual time needed to complete: 6 minutes.
--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
+import HornedBeastData from "./data.json"
 
 class Main extends React.Component {
   render() {
@@ -8,14 +9,28 @@ class Main extends React.Component {
         <h1>
           Welcome to the Sickest Horned Animal Image Collation Site.
         </h1>
-        <HornedBeast
-          title={"Buffalo"}
-          description={"Probably a Water Buffalo, but I'm not confident enough to title it that."}
-          imageUrl={"https://images.pexels.com/photos/1054650/pexels-photo-1054650.jpeg?cs=srgb&dl=pexels-harvey-sapir-1054650.jpg&fm=jpg"} />
-        <HornedBeast
-          title={"Unicorn?"}
-          description={"...Are you doubting me?"}
-          imageUrl={"https://images.pexels.com/photos/1564506/pexels-photo-1564506.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"} />
+        <div className="beast-flex">
+          {/* Horned Beasts I added... */}
+          <HornedBeast
+            title={"Buffalo"}
+            description={"Probably a Water Buffalo, but I'm not confident enough to title it that."}
+            imageUrl={"https://images.pexels.com/photos/1054650/pexels-photo-1054650.jpeg?cs=srgb&dl=pexels-harvey-sapir-1054650.jpg&fm=jpg"} />
+          <HornedBeast
+            title={"Unicorn?"}
+            description={"...Are you doubting me?"}
+            imageUrl={"https://images.pexels.com/photos/1564506/pexels-photo-1564506.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"} />
+          <HornedBeast
+            title={"Antelope"}
+            description={"Elegant, aren't they?"}
+            imageUrl={"https://images.pexels.com/photos/52961/antelope-nature-flowers-meadow-52961.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"} />
+          {/* And Horned Beasts Parsed from Data. */}
+          {HornedBeastData.map(beastData => {
+            return (<HornedBeast
+              title={beastData.title}
+              description={beastData.description}
+              imageUrl={beastData.image_url} />
+          )})}
+        </div>
       </>
     )
   }
